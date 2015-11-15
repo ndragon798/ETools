c02d7cca1d38bd1fb631e0174a82700c000f180a
ndragon798 <ndragon798@yahoo.com>
2015-11-15 00:43:42 -0500

    Init

---
+++ ETools/Form1.Designer.vb
@@ -0,0 +1,411 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GueAcc = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.pasCom = New System.Windows.Forms.Label()
+        Me.pasRem = New System.Windows.Forms.Label()
+        Me.minPasLeg = New System.Windows.Forms.Label()
+        Me.maxPasAge = New System.Windows.Forms.Label()
+        Me.minPasAge = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(783, 370)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.LinkLabel1)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.Label8)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.Panel1)
+        Me.TabPage1.Controls.Add(Me.Label7)
+        Me.TabPage1.Controls.Add(Me.GueAcc)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.RadioButton2)
+        Me.TabPage1.Controls.Add(Me.RadioButton1)
+        Me.TabPage1.Controls.Add(Me.NumericUpDown4)
+        Me.TabPage1.Controls.Add(Me.NumericUpDown3)
+        Me.TabPage1.Controls.Add(Me.NumericUpDown2)
+        Me.TabPage1.Controls.Add(Me.NumericUpDown1)
+        Me.TabPage1.Controls.Add(Me.pasCom)
+        Me.TabPage1.Controls.Add(Me.pasRem)
+        Me.TabPage1.Controls.Add(Me.minPasLeg)
+        Me.TabPage1.Controls.Add(Me.maxPasAge)
+        Me.TabPage1.Controls.Add(Me.minPasAge)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(775, 344)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Password Config"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(697, 298)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(46, 24)
+        Me.Button2.TabIndex = 25
+        Me.Button2.Text = "Run"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(645, 298)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(46, 24)
+        Me.Button1.TabIndex = 24
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(478, 309)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 23
+        Me.Label8.Text = "Label8"
+        Me.Label8.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(478, 294)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 22
+        Me.Label6.Text = "Label6"
+        Me.Label6.Visible = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.RadioButton3)
+        Me.Panel1.Controls.Add(Me.RadioButton4)
+        Me.Panel1.Location = New System.Drawing.Point(158, 294)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(131, 44)
+        Me.Panel1.TabIndex = 21
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(29, 11)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton3.TabIndex = 18
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "On"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(74, 11)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton4.TabIndex = 19
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Off"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(295, 309)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(99, 13)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Recommended: Off"
+        '
+        'GueAcc
+        '
+        Me.GueAcc.AutoSize = True
+        Me.GueAcc.Location = New System.Drawing.Point(26, 309)
+        Me.GueAcc.Name = "GueAcc"
+        Me.GueAcc.Size = New System.Drawing.Size(78, 13)
+        Me.GueAcc.TabIndex = 17
+        Me.GueAcc.Text = "Guest Account"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(429, 33)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(331, 230)
+        Me.TextBox1.TabIndex = 16
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(295, 41)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Recommended: 1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(295, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Recommended: 90"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(293, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Recommended: 8"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(293, 207)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Recommended: 24"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(293, 261)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Recommended: On"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(232, 259)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Off"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(187, 259)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "On"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(187, 200)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(35, 20)
+        Me.NumericUpDown4.TabIndex = 8
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(187, 145)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {50, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(35, 20)
+        Me.NumericUpDown3.TabIndex = 7
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(187, 88)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {120, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(35, 20)
+        Me.NumericUpDown2.TabIndex = 6
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(187, 34)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {120, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(35, 20)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'pasCom
+        '
+        Me.pasCom.AutoSize = True
+        Me.pasCom.Location = New System.Drawing.Point(26, 261)
+        Me.pasCom.Name = "pasCom"
+        Me.pasCom.Size = New System.Drawing.Size(106, 13)
+        Me.pasCom.TabIndex = 4
+        Me.pasCom.Text = "Password Complexity"
+        '
+        'pasRem
+        '
+        Me.pasRem.AutoSize = True
+        Me.pasRem.Location = New System.Drawing.Point(26, 207)
+        Me.pasRem.Name = "pasRem"
+        Me.pasRem.Size = New System.Drawing.Size(124, 13)
+        Me.pasRem.TabIndex = 3
+        Me.pasRem.Text = "Passwords Remembered"
+        '
+        'minPasLeg
+        '
+        Me.minPasLeg.AutoSize = True
+        Me.minPasLeg.Location = New System.Drawing.Point(26, 152)
+        Me.minPasLeg.Name = "minPasLeg"
+        Me.minPasLeg.Size = New System.Drawing.Size(133, 13)
+        Me.minPasLeg.TabIndex = 2
+        Me.minPasLeg.Text = "Minimum Password Length"
+        '
+        'maxPasAge
+        '
+        Me.maxPasAge.AutoSize = True
+        Me.maxPasAge.Location = New System.Drawing.Point(26, 95)
+        Me.maxPasAge.Name = "maxPasAge"
+        Me.maxPasAge.Size = New System.Drawing.Size(120, 13)
+        Me.maxPasAge.TabIndex = 1
+        Me.maxPasAge.Text = "Maximum password age"
+        '
+        'minPasAge
+        '
+        Me.minPasAge.AutoSize = True
+        Me.minPasAge.Location = New System.Drawing.Point(26, 41)
+        Me.minPasAge.Name = "minPasAge"
+        Me.minPasAge.Size = New System.Drawing.Size(117, 13)
+        Me.minPasAge.TabIndex = 0
+        Me.minPasAge.Text = "Minimum password age"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(701, 266)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(32, 13)
+        Me.LinkLabel1.TabIndex = 26
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "V 1.0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(776, 370)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "ETools"
+        Me.TabControl1.ResumeLayout(false)
+        Me.TabPage1.ResumeLayout(false)
+        Me.TabPage1.PerformLayout
+        Me.Panel1.ResumeLayout(false)
+        Me.Panel1.PerformLayout
+        CType(Me.NumericUpDown4,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.NumericUpDown3,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.NumericUpDown2,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.NumericUpDown1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+
+End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents pasCom As System.Windows.Forms.Label
+    Friend WithEvents pasRem As System.Windows.Forms.Label
+    Friend WithEvents minPasLeg As System.Windows.Forms.Label
+    Friend WithEvents maxPasAge As System.Windows.Forms.Label
+    Friend WithEvents minPasAge As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents GueAcc As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ ETools/Form1.vb
@@ -0,0 +1,214 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim NF_MinPassWord_Leg As String
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs) Handles TabPage1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim CmdStr As String
+        CmdStr = "/export /cfg c:\cfg.txt"
+        Dim myProcess As System.Diagnostics.Process
+        myProcess = New System.Diagnostics.Process()
+        myProcess.StartInfo.FileName = "C:\Windows\System32\SecEdit.exe"
+        myProcess.StartInfo.Arguments = CmdStr
+        myProcess.Start()
+        Threading.Thread.Sleep(2000)
+        Dim EntireFile As String
+        Dim oFile As System.IO.File
+        Dim oRead As System.IO.StreamReader
+        oRead = File.OpenText("C:\cfg.txt")
+
+        EntireFile = oRead.ReadToEnd()
+        TextBox1.Text = (EntireFile)
+
+        'PASSWORD COMPLEXTIY SET
+        If TextBox1.Text.Contains("PasswordComplexity = 0") Then
+            RadioButton2.Checked = True
+        ElseIf TextBox1.Text.Contains("PasswordComplexity = 1") Then
+            RadioButton1.Checked = True
+        End If
+        '/END PASSWORD COMPLETIXTY SET
+
+        'GUESTACCOUNT SET
+        If TextBox1.Text.Contains("EnableGuestAccount = 0") Then
+            RadioButton4.Checked = True
+        ElseIf TextBox1.Text.Contains("EnableGuestAccount = 1") Then
+            RadioButton3.Checked = True
+        End If
+        '/END GUEST ACCOUNT SET
+
+        'MIN PASSWORD LENGTH SET 
+        Dim MinLeg As String
+        Dim MinLegpp As String
+        For MinLegp As Integer = -1000 To 1000
+
+            MinLeg = "MinimumPasswordLength = "
+            MinLegpp = CStr(MinLegp)
+            MinLeg = MinLeg & MinLegpp
+            If TextBox1.Text.Contains(MinLeg) Then
+                NumericUpDown3.Value = MinLegp
+            End If
+        Next
+        Dim NumUpDown3 As Integer = NumericUpDown3.Value
+        NF_MinPassWord_Leg = "MinimumPasswordLength = " + CStr(NumUpDown3)
+        'Label6.Text = NF_MinPassWord_Leg
+
+        '/END MIN PASSWORD LENGTHSET
+
+        'MIN PASSWORD AGE SET
+        Dim MinAge As String
+        For MinAgep As Integer = -1000 To 1000
+            MinAge = "MinimumPasswordAge = "
+            MinAge = MinAge & CStr(MinAgep)
+            If TextBox1.Text.Contains(MinAge) Then
+                If MinAgep < 9 & MinAgep > 0 Then
+                    For a As Integer = 0 To 9
+                        If TextBox1.Text.Contains(MinAge & CStr(a)) Then
+                            NumericUpDown1.Value = Convert.ToInt32(CStr(MinAgep) & CStr(a))
+                        Else
+                            NumericUpDown1.Value = MinAgep
+                        End If
+                    Next
+                    NumericUpDown1.Value = MinAgep
+                End If
+
+
+            End If
+
+        Next
+        '/END MIN PASSWORD AGE SET
+        'MAX PASSWORD AGE SET
+        Dim MaxAge As String
+        For MaxAgep As Integer = -1000 To 1000
+            MaxAge = "MaximumPasswordAge = "
+            MaxAge = MaxAge & CStr(MaxAgep)
+            If TextBox1.Text.Contains(MaxAge) Then
+                NumericUpDown2.Value = MaxAgep
+            End If
+
+        Next
+        '/END MAX PASSWORD AGE SET
+
+        'PASSWORDS REMEMBERED SET
+        Dim PasRem As String
+        For PasRemp As Integer = -1000 To 1000
+            PasRem = "PasswordHistorySize = "
+            PasRem = PasRem & CStr(PasRemp)
+            If TextBox1.Text.Contains(PasRem) Then
+                NumericUpDown4.Value = PasRemp
+            End If
+
+        Next
+        '/END PASSWORDS REMEMBERED SET
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        'ON PASSWORD COMPLEXITY
+        TextBox1.Text = TextBox1.Text.Replace("PasswordComplexity = 0", "PasswordComplexity = 1")
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        'OFF PASSWORD COMPLEXITY
+        TextBox1.Text = TextBox1.Text.Replace("PasswordComplexity = 1", "PasswordComplexity = 0")
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        'OFF GUEST ACCOUNT
+        TextBox1.Text = TextBox1.Text.Replace("EnableGuestAccount = 1", "EnableGuestAccount = 0")
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        'ON GUEST ACCOUNT
+        TextBox1.Text = TextBox1.Text.Replace("EnableGuestAccount = 0", "EnableGuestAccount = 1")
+    End Sub
+
+    Private Sub NumericUpDown3_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown3.ValueChanged
+        'NUMERIC UPDOWN FOR MIN PASSWORD LENGTH
+        Dim l As String
+
+        For x As Integer = -1000 To 1000
+            l = "MinimumPasswordLength = " + CStr(x)
+
+            If TextBox1.Text.Contains(l) Then
+                TextBox1.Text = TextBox1.Text.Replace(l, "MinimumPasswordLength = " + CStr(NumericUpDown3.Value))
+            End If
+
+        Next
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        'NUMERIC UPDOWN FOR MIN PASSWORD AGE
+        Dim l As String
+
+        For x As Integer = -1000 To 1000
+            l = "MinimumPasswordAge = " + CStr(x)
+
+            If TextBox1.Text.Contains(l) Then
+                TextBox1.Text = TextBox1.Text.Replace(l, "MinimumPasswordAge = " + CStr(NumericUpDown1.Value))
+            End If
+
+        Next
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        'NUMERIC UPDWON FOR MAX PASSWORD AGE
+        Dim l As String
+
+        For x As Integer = -1000 To 1000
+            l = "MaximumPasswordAge = " + CStr(x)
+
+            If TextBox1.Text.Contains(l) Then
+                TextBox1.Text = TextBox1.Text.Replace(l, "MaximumPasswordAge = " + CStr(NumericUpDown2.Value))
+            End If
+
+        Next
+    End Sub
+
+    Private Sub NumericUpDown4_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown4.ValueChanged
+        Dim l As String
+
+        For x As Integer = -1000 To 1000
+            l = "PasswordHistorySize = " + CStr(x)
+
+            If TextBox1.Text.Contains(l) Then
+                TextBox1.Text = TextBox1.Text.Replace(l, "PasswordHistorySize = " + CStr(NumericUpDown4.Value))
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        File.Create("C:\ncfg.inf").Dispose()
+        Dim FILE_NAME As String = "c:\ncfg.inf"
+        Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+        objWriter.Write(TextBox1.Text)
+        objWriter.Close()
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim CmdStr As String
+        CmdStr = "/configure /db secedit.sdb /cfg ""c:\ncfg.inf"" /quiet"
+        Dim myProcess As System.Diagnostics.Process
+        myProcess = New System.Diagnostics.Process()
+        myProcess.StartInfo.FileName = "C:\Windows\System32\SecEdit.exe"
+        myProcess.StartInfo.Arguments = CmdStr
+        myProcess.Start()
+
+        CmdStr = "/refreshpolicy machine_policy /enforce /quiet"
+
+        myProcess = New System.Diagnostics.Process()
+        myProcess.StartInfo.FileName = "C:\Windows\System32\SecEdit.exe"
+        myProcess.StartInfo.Arguments = CmdStr
+        myProcess.Start()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://www.nathaneaston.com/ETools/")
+    End Sub
+End Class
